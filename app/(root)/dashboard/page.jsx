b02cc146056d42ed6b0ef3328f49511464370a78
YashGupta2116 <yashgupta2116@gmail.com>
2025-09-05 14:08:40 +0530

    Formatted the file

--- app/(root)/dashboard/page.jsx
+++ app/(root)/dashboard/page.jsx
@@ -1,12 +1,12 @@
-import Navbarblock from '@/components/Navbar'
-import React from 'react'
+import Navbarblock from "@/components/Navbar";
+import React from "react";
 
 const page = () => {
-    return (
-        <div className='w-screen h-screen bg-gradient-to-br from-[#0F2027] via-[#2C5364] to-[#2C5364]'>
-            <Navbarblock/>
-        </div>
-    )
-}
+  return (
+    <div className="w-screen h-screen bg-gradient-to-br from-[#0F2027] via-[#2C5364] to-[#2C5364]">
+      <Navbarblock />
+    </div>
+  );
+};
 
-export default page
+export default page;
