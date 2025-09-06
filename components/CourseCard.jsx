d8dc43861722103afd1105f7ec4f6bd036e43df5
Aryanluthra1911 <aryanluthra1911@gmail.com>
2025-09-06 06:27:58 +0530

    fixess

--- components/CourseCard.jsx
+++ components/CourseCard.jsx
@@ -93,21 +93,13 @@ const CourseCard = ({ course }) => {
         <div className="w-full h-[85%] flex justify-around">
           <div className="w-[80%] h-full">
             <div className="text-xl font-semibold">Learn {title}</div>
-            <div className="flex h-5 w-40 text-xs text-[#9a9a9a]">
-              {lessons} lessons
-            </div>
-            <div className="text-md text-[#9a9a9a]">Rating: {rating}</div>
           </div>
           <div className="w-[20%] h-full flex justify-end">
             <img src={src} alt="" className="w-12 h-12" />
           </div>
         </div>
         <div className="h-[15%] w-full flex gap-2">
-          <div className="flex text-xs text-[#9a9a9a] h-full w-[25%] items-center gap-3">
-            <img src="/people.svg" alt="" className="h-3 w-3" />
-            {enrolled}
-          </div>
-          <div className="flex text-xs text-[#9a9a9a] h-full w-[30%] items-center gap-1">
+          <div className="flex text-xs text-[#9a9a9a] h-full w-[40%] items-center gap-1">
             <img src="/clock.svg" alt="" className="h-3 w-3" />
             {duration} hours
           </div>
